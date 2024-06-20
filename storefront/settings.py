db2581a92159df2cfdeba7c2f891e4dee5e27ea1
hardik-appscrip <kothiyahardikkumar@appscrip.co>
2024-06-20 12:21:43 +0530

    Setup SQL Connection

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': '127.0.0.1',
+        'USER': 'hardik',
+        'PASSWORD': 'Hardik@991322',
+        'PORT': '3306'
     }
 }
 
