8191d185a85ba034627fce5af2568ef9ddaf8103
hardik-appscrip <kothiyahardikkumar@appscrip.co>
2024-06-20 11:35:34 +0530

    Data Migrations

--- store/models.py
+++ store/models.py
@@ -17,8 +17,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)  # 9999.99
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)  # 9999.99
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -43,6 +44,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'],name="idx_firstname_lastname")
+        ]
+   
 
 
 class Order(models.Model):
